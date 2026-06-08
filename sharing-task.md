f09adf793c374e012b381a18e35cb389449912b7
Marcandy <marcandy.nj@gmail.com>
2026-06-08 15:48:53 -0400

    group practice solution

--- sharing-task.md
+++ sharing-task.md
@@ -19,19 +19,25 @@ function calculateDiscount(price, discountRate) {
     if (typeof price !== 'number' || typeof discountRate !== 'number') return null;
     if (discountRate < 0 || discountRate > 1) return null;
     // TODO: Implement logic
-    return null;
+    const discountedP = price * discountRate;
+
+    return price - discountedP;
 }
 
 function filterProducts(products, callback) {
     if (!Array.isArray(products) || typeof callback !== 'function') return [];
     // TODO: Implement filtering logic
-    return [];
+
+    const filterProd = products.filter(callback);
+    return filterProd;
 }
 
 function sortInventory(inventory, key) {
     if (!Array.isArray(inventory) || typeof key !== 'string') return [];
     // TODO: Implement sorting logic
-    return [];
+
+    
+    return inventory.includes(key);
 }
 ```
 
---
+++ test/unitest.test.js
@@ -0,0 +1,55 @@
+// import { calculateDiscount } from "../unitest-practice";
+const { calculateDiscount, filterProducts, sortInventory } = require('../unitest-practice.js');
+
+console.log(calculateDiscount, 'here --')
+
+test("applies a valid discount rate", function() {
+    expect(calculateDiscount(100, 0.1)).toBe(90);
+});
+
+test("discount price", ()=> {
+    expect(calculateDiscount(70, 0.2)).toBe(56);
+})
+
+test('negative discount', ()=> {
+    expect(calculateDiscount(70, 4)).toBe(null);
+})
+
+test('discount edge case', () => {
+    expect(calculateDiscount()).toBe(null);
+})
+
+
+
+const testArr = ['park', 'running', 'school', 'fly', 'hel'];
+
+
+test('fileter product', () => {
+    expect(filterProducts(testArr, (prod) => prod.length < 4 )).toStrictEqual(['fly', 'hel'])
+
+})
+
+test('fileter product', () => {
+    expect(filterProducts(testArr, (prod) => prod === 'beach' )).toStrictEqual([]);
+
+})
+
+test('fileter product', () => {
+    expect(filterProducts('', (prod) => prod  )).toStrictEqual([]);
+
+})
+
+
+const inventory = ['park','school', 'fly', 'hel'];
+
+test("positive invetory test", () => {
+    expect(sortInventory(inventory, 'park')).toBe(true);
+});
+
+test("invetory test", () => {
+    expect(sortInventory(inventory, 'village')).toBe(false);
+});
+
+test("invetory test", () => {
+    expect(sortInventory(inventory, [])).toStrictEqual([]);
+});
